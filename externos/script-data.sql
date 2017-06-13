f4b79b86e61f130cefaf18b4f0fb2a3044106fde
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2017-06-13 00:09:14 -0300

    Ajustes na base de dados para multiplos treinos

--- externos/script-data.sql
+++ externos/script-data.sql
@@ -24,26 +24,29 @@ insert into schedule (week_day, schedule_start, schedule_end) values (4, '16:00'
 /*Training Type==================================================================================*/
 select * from training_type;
 
+insert into training_type (name) values ('Aquecimento');
+insert into training_type (name) values ('Técnica');
 insert into training_type (name) values ('WOD 1');
 insert into training_type (name) values ('WOD 2');
 insert into training_type (name) values ('WOD 3');
-insert into training_type (name) values ('AMRAP');
-insert into training_type (name) values ('Aquecimento');
-insert into training_type (name) values ('Técnica');
+
+
 /*==========================================================================================*/
 
 /*Movement==================================================================================*/
 select * from movement;
 
-insert into movement (name, translate, description) values ('Burpee','Burpee','Descrição do Burpee');
+insert into movement (name, translate, description) values ('Scketch','Scketch','Descrição Scketch');
 insert into movement (name, translate, description) values ('Single Under','Corda simples','Descrição Single Under');
 insert into movement (name, translate, description) values ('Double Under','Corda dupla','Descrição Double Under');
+insert into movement (name, translate, description) values ('Jumping Jacks','Jumping Jacks','Descrição Jumping Jacks');
+insert into movement (name, translate, description) values ('Burpee','Burpee','Descrição do Burpee');
 insert into movement (name, translate, description) values ('Thruster','Thruster','Descrição Thruster');
 insert into movement (name, translate, description) values ('Wall Ball','Bola na Parede','Descrição Wall Ball');
 insert into movement (name, translate, description) values ('Pull Up','Barra','Descrição Pull Up');
 insert into movement (name, translate, description) values ('Push Up','Flexão de braço','Descrição Push Up');
 insert into movement (name, translate, description) values ('Clean and Jerk','Clean and Jerk','Descrição Clean and Jeark');
-insert into movement (name, translate, description) values ('Jumping Jacks','Jumping Jacks','Descrição Jumping Jacks');
+
 
 /*==========================================================================================*/
 
@@ -52,9 +55,8 @@ insert into movement (name, translate, description) values ('Jumping Jacks','Jum
 select * from training;
 select * from training_movement;
 
-insert into training (training_type_id, date, qt_round) values (1, '2017-05-31', 3);
-insert into training_movement values (1, 1, 10);
-insert into training_movement values (4, 1, 20);
+delete from training_movement where movement_id > 0;
+delete from training where id > 0;
 
 select  t.date data_treino,
 		tt.name treino,
@@ -68,8 +70,8 @@ from 	training t,
 where t.id = tm.training_id
 and t.training_type_id = tt.id
 and tm.movement_id = m.id
-and t.date = '2017-06-08'
-order by t.date, m.name;
+and t.date = '2017-06-12'
+order by t.date, t.sequence, m.name;
 
 /*==========================================================================================*/
 
